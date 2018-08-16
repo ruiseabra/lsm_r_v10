2a772c60fd3d0d3f41f53d15c0c7be45a86cfdda
ruiseabra <ruisea@gmail.com>
2018-08-16 15:04:01 +0000

    get.meteo: - produces a xts instead of a tibble - now includes interpolation to several most used dt - now changes rain units to per sec (was per h), and already ensures no negative values are present - now pressure is in Pa (was hPa) - now includes tide height (but conversion to tide in/tide out must be done later) - now saves files to a "io" folder in the parent directory, so that it can be used by all "child" scripts FUNS.forcing (was FUNS.env_tide): - tides function moved into get.meteo - most elements of the job moved into get.meteo - now only loads the weather data at the right interpolation step and computes tide in/tide out
    FURTHER CHANGES NEEDED DOWN THE PIPELINE TO DEAL WITH THESE CHANGES

--- get.meteo.R
+++ get.meteo.R
@@ -1,16 +1,80 @@
 Sys.setenv(TZ = "UTC")
 options(digits = 10)
-suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(lubridate))
-suppressPackageStartupMessages(library(stringr))
-suppressPackageStartupMessages(library(meteoForecast))
+library(tidyverse)
+library(lubridate)
+library(stringr)
+library(xts)
+library(meteoForecast)
+
+########################-
+## collect tide data ###-
+########################-
+# lon
+# lat
+# T_RANGE = POSIXct date:time range (2 values)
+# DT = every 'DT' seconds
+########################-
+fes.tides <- function(lat, lon, T_RANGE, DT) {
+	Sys.setenv(HDF5_DISABLE_VERSION_CHECK = "2")
+	# prepare
+	ORIGIN  <- ymd("1950-01-01")
+	t_range <- (T_RANGE + c(0, DT)) %>%
+		julian(origin = ORIGIN) %>%
+		as.numeric %>%
+		formatC(format = "f")
+	DT <- DT / 60
+	CALL  <- str_c("fes_slev", lat, lon, t_range[1], t_range[2], DT, sep = " ")
+	# run fes_slev
+	tides <- system(CALL, intern = TRUE)[-(1:2)] %>% str_split(",")
+	# extract timestamps
+	times <- map_chr(tides, 1) %>%
+		as.numeric %>%
+		"*"(., (24 * 3600)) %>%
+		round
+	times <- (times - (times %% 60)) %>%
+		as.POSIXct(origin = ORIGIN)
+	times <- times - (as.numeric(times) %% 60)
+	steady <- diff(times) %>%
+		unique %>%
+		length %>%
+		"=="(., 1)
+	if (!steady) stop("the period of 'times' is irregular")
+	# extract tide elevation
+	tides <- map_chr(tides, 2) %>% as.numeric
+	# combine
+	tides <- tibble(time = times, tide = tides)
+	# filter to ensure that the data return does not exceed the t_range supplied
+	tides <- filter(tides, time %within% interval(T_RANGE[1], T_RANGE[2]))
+	# return
+	tides
+}
+########################-
+########################-
+########################-
+
+DIR <- getwd() %>% dirname %>% str_c("/io/weather/")
+dir.create(DIR, showWarnings = FALSE, recursive = TRUE)
+
+### generate a xts with the following columns (colnames must be respected):
+## index = time (YYYY-mm-dd HH:MM:SS)
+# wind - wind speed (m/s)
+# air  - surface air temperature (K)
+# sst  - sea surface temperature (K)
+# sw   - incoming short wave radiation (W/m2)
+# lw   - incoming  long wave radiation (W/m2)
+# rh   - relative humidity (0-100%)
+# pres - surface pressure (Pa)
+# rain - precipitation rate (mm/s, kg/m2/s) (always >= 0)
+# wave - NOT YET IMPLEMENTED sig or max wave height (m)
 
 # var <- grepVar("", "meteogalicia", day = Sys.Date() - 15, complete = TRUE)
 # var[29,]
 
-load("weather.Rdata")
-w2 <- w; rm(w)
+# interpolation intervals
+dt <- NULL
+dt <- c(1800, 900)
 
+### collect data ####
 # define service provider
 mfService("meteogalicia")
 
@@ -22,11 +86,12 @@ lcc <- mfProj4("meteogalicia")
 
 # target variables
 vars <- c("lwm","mod","temp","sst","swflx","lwflx","rh","mslp","prec")
+cols <- c("wind","air","sst","sw","lw","rh","pres","rain")
 
 t0 <- ymd_h("2017-10-10 00")
 t1 <- ymd_h("2018-03-01 00")
 lat <- 41.84
-lon <- -8.85 # -8.85 is the eastermost (coastalmost) sea pixel
+lon <- -8.89 # -8.85 is the eastermost (coastalmost) sea pixel in the meteogalicia model, but -8.89 is the easternmost sea pixel to get fes tides
 
 spoint <- SpatialPoints(cbind(lon, lat))
 projection(spoint) <- wgs84
@@ -35,30 +100,47 @@ spoint <- spTransform(spoint, lcc)
 
 # get data at coordinates
 w <- getPointDays(spoint,vars = vars, start = t0, end = t1, resolution = 4)
+w <- w[time(w) >= t0 & time(w) <= t1, ]
+t0 <- first(time(w))
+t1 <- last(time(w))
+colnames(w) <- c("lwm", cols)
 
-# change units and re-arrange data
+### check quality ####
+# check that the pixel selected fall in water (== 1)
+if (!w$lwm[1]) stop("inland location")
+w <- w[,-1]
 
-### must be a tibble with the following columns (colnames must be respected):
-# time - YYYY-mm-dd HH:MM
-# wind - wind speed (m/s)
-# air  - surface air temperature (K)
-# sst  - sea surface temperature (K)
-# sw   - incoming short wave radiation (W/m2)
-# lw   - incoming  long wave radiation (W/m2)
-# rh   - relative humidity (%)
-# pres - surface pressure (hPa)
-# rain - precipitation rate (mm/h, kg/m2/h)
-# wave - NOT YET IMPLEMENTED sig or max wave height (m)
+# fun DQSDT (within SatMix) is only valid for air temperatures between 173 and 373 K (-100 to +100 C)
+if (any(w$air < 173 | w$air > 373)) stop("DQSDT")
 
-w <- tibble(
-  time = time(w),
-  wind = round(w$mod,   2),
-  air  = round(w$temp,  1),
-  sst  = round(w$sst,   1),
-  sw   = round(w$swflx, 1),
-  lw   = round(w$lwflx, 1),
-  rh   = round(w$rh   * 100, 1),
-  pres = round(w$mslp / 100, 0),
-  rain = round(ifelse(w$prec < 0, 0, w$prec), 2)) %>% mutate_at(-1, as.numeric)
+# negative values for precipitation may occur, but they cannot be fed into the lsm model
+w$rain <- ifelse(w$rain < 0, 0, w$rain)
+w$rain <- w$rain / 3600
 
-save(w, file = "weather.RData")
+### get tide ####
+tides <- fes.tides(lat, lon, c(t0, t1), 3600)$tide
+# barometric pressure correction (+10 hPa = -10 cm; REF = 1013 hPa)
+# this approach is crude but can be extended to the future as well as the past, with the same level of bias
+tides <- tides - ((w$pres / 100) - 1013)
+w$tide <- tides
+
+# future # include waverunup
+# future # include atmospheric effects (pressure, wind surge)
+
+### clean up ####
+w <- xts(coredata(w), time(w))
+save(w, file = str_c(DIR, "weather_3600.RData"))
+w2 <- w
+
+### interpolate to other dt ####
+T0 <- time(w2)
+if (!any(is.null(dt))) {
+	for (t in dt) {
+		T1 <- seq.POSIXt(t0, t1, by = t)
+		w  <- apply(coredata(w2), 2, function(x) approx(T0, x, T1, method = "linear", rule = 2)$y)
+		w <- xts(w, T1)
+		tides <- fes.tides(lat, lon, c(t0, t1), t)$tide
+		w$tide <- tides - ((w$pres / 100) - 1013)
+		save(w, file = str_c(DIR, "weather_", t, ".RData"))
+	}
+}
